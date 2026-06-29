f0631d73a5232156f2a7229b4c3979abb78588be
Renan Araujo Polainas <re.araujo.po@gmail.com>
2026-06-29 19:31:50 -0300

    tela pausa

---
+++ screens/pausa/pausa.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_retornar_pressed() -> void:
+	pass
+
+
+func _on_fases_pressed() -> void:
+	pass
+
+
+func _on_resetar_pressed() -> void:
+	pass
+
+
+func _on_menu_pressed() -> void:
+	pass
